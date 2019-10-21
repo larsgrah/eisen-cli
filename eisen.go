e99b13e0562b47a24f35e093eda31664036c66db
Lars Grahmann <lars@kloud.software>
2019-10-21 22:50:15 +0200

    migrate to tailwind, updates

--- eisen.go
+++ eisen.go
@@ -140,10 +140,14 @@ export class ` + componentName + ` extends Component {
     "clean": "rm -rf dist/ && rm -rf lib/" },
   "author": "",
   "license": "ISC",
-  "dependencies": {
-    "@kloudsoftware/eisen": "1.0.25"
+    "dependencies": {
+    "@kloudsoftware/eisen": "1.0.28",
+    "postcss": "^7.0.18",
+    "tailwindcss": "^1.1.2"
   },
   "devDependencies": {
+    "parcel-bundler": "^1.12.4",
+    "parcel-plugin-purgecss": "^2.1.2",
     "parcel-plugin-static-files-copy": "^2.0.0",
     "sass": "^1.18.0",
     "typescript": "^3.4.2"
@@ -175,6 +179,52 @@ CMD ["nginx", "-g", "daemon off;"]`)
 			err = ioutil.WriteFile("Dockerfile", dockerfile, 0644)
 			check(err)
 
+			postcss := []byte(`module.exports = {
+    plugins: [
+        require('tailwindcss'),
+        require('autoprefixer'),
+    ]
+};`)
+
+			purgecss := []byte(`module.exports = {
+    content: ["**/*.ts"],
+};`)
+			tailwindcss := []byte(`module.exports = {
+};`)
+			tsconfig := []byte(`{
+  "compilerOptions": {
+    "module": "commonjs",
+    "moduleResolution": "node",
+    "newLine": "LF",
+    "outDir": "./lib/",
+    "target": "es5",
+    "sourceMap": true,
+    "declaration": true,
+    "jsx": "preserve",
+    "lib": [
+      "es2017",
+      "dom",
+      "esnext"
+    ]
+  },
+  "include": [
+    "src/**/*"
+  ],
+  "exclude": [
+    ".git",
+    "node_modules"
+  ]
+}`)
+
+			err = ioutil.WriteFile("postcss.config.js", postcss, 0644)
+			check(err)
+			err = ioutil.WriteFile("purgecss.config.js", purgecss, 0644)
+			check(err)
+			err = ioutil.WriteFile("tailwind.config.js", tailwindcss, 0644)
+			check(err)
+			err = ioutil.WriteFile("tsconfig.json", tsconfig, 0644)
+			check(err)
+
 			err = os.Mkdir("src", os.ModePerm)
 			err = os.Chdir("src")
 
@@ -190,178 +240,12 @@ CMD ["nginx", "-g", "daemon off;"]`)
 </body>
 
 <script src="./index.ts"></script>`)
-			stylescss := []byte(`@import 'reset.scss';
+			stylescss := []byte(`
+@tailwind base;
 
-@mixin transform($property) {
-    -webkit-transform: $property;
-    -ms-transform: $property;
-    -moz-transform: $property;
-    transform: $property;
-}
+@tailwind components;
 
-$transparent: transparent;
-
-$white: #FFFFFF;
-$grey: #B8C2CC;
-$grey-lightest: #f8f8f8;
-$grey-lighter: #efefef;
-$grey-light: #dadada;
-$grey-dark: #8795A1;
-$grey-darker: #949ea8;
-$grey-darkest: #3D4852;
-$black: #22292F;
-
-$blue: #3490DC;
-$blue-lightest: #EFF8FF;
-$blue-lighter: #ccf0ff;
-$blue-light: #3490dc;
-$blue-dark: #2779BD;
-$blue-darker: #266fb3;
-$blue-darkest: #12283A;
-
-$red: #B94848;
-$red-lightest: #fcebea;
-$red-lighter: #f9acaa;
-$red-light: #EF5753;
-$red-dark: #9b4e4e;
-$red-darker: #621B18;
-$red-darkest: #3B0D0C;
-
-$orange: #F6993F;
-$orange-lightest: #FFF5EB;
-$orange-lighter: #FCD9B6;
-$orange-light: #FAAD63;
-$orange-dark: #DE751F;
-$orange-darker: #613B1F;
-$orange-darkest: #462A16;
-
-$green: #38C172;
-$green-lightest: #E3FCEC;
-$green-lighter: #A2F5BF;
-$green-light: #51D88A;
-$green-dark: #1F9D55;
-$green-darker: #1A4731;
-$green-darkest: #0F2F21;
-
-$border-radius-none: 0;
-$border-radius-sm: .125rem;
-$border-radius-default: .25rem;
-$border-radius-lg: .5rem;
-$border-radius-full: 9999px;
-
-.user-input-label {
-    padding-bottom: .2em;
-    padding-top:.4em;
-}
-
-.user-input {
-    margin-bottom:.5em;
-    -moz-appearance: none;
-    -webkit-appearance: none;
-    -webkit-box-align: center;
-    -ms-flex-align: center;
-    align-items: center;
-    border: none;
-    border-radius: 3px;
-    box-shadow: none;
-    display: -webkit-inline-box;
-    display: -ms-inline-flexbox;
-    display: inline-flex;
-    font-size: 1rem;
-    height: 2.25em;
-    -webkit-box-pack: start;
-    -ms-flex-pack: start;
-    justify-content: flex-start;
-    line-height: 1.25;
-    padding-bottom: .5em;
-    padding-left: .625em;
-    padding-right: .625em;
-    padding-top: .5em;
-    position: relative;
-    vertical-align: top;
-    background-color: #fff;
-    border: 1px solid #dbdbdb;
-    color: #363636;
-    //box-shadow: inset 0 1px 2px rgba(10,10,10,.1);
-    max-width: 100%;
-    width: 100%;
-}
-
-.card {
-    box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
-}
-
-.btn {
-    border-radius: $border-radius-default;
-    text-decoration:none;
-	  text-shadow:0px 1px 0px $green-darker;
-}
-
-.btn:hover {
-    filter: brightness(110%);
-}
-
-.btn-confirm {
-    background-color: $green;
-    border-color: darkgreen;
-    color: white;
-}
-
-.error {
-    border-color: red;
-}
-h1, h2, h3, h4, h5, h6 {
-    font-weight: 100;
-    margin: 0;
-}
-
-`)
-
-			resetscss := []byte(`html, body, div, span, applet, object, iframe, p, blockquote, pre, a, abbr, acronym, address, big, cite, code, del, dfn, em, img, ins, kbd, q, s, samp, small, strike, strong, sub, sup, tt, var, b, u, i, center, dl, dt, dd, ol, ul, li, fieldset, form, label, legend, table, caption, tbody, tfoot, thead, tr, th, td, article, aside, canvas, details, embed, figure, figcaption, footer, header, hgroup, menu, nav, output, ruby, section, summary, time, mark, audio, video {
-    margin: 0;
-    padding: 0;
-    border: 0;
-    vertical-align: baseline; 
-}
-
-
-article, aside, details, figcaption, figure, footer, header, hgroup, menu, nav, section {
-    display: block; 
-}
-
-body {
-    line-height: 1; 
-}
-
-blockquote, q {
-    quotes: none; 
-}
-
-blockquote {
-    &:before, &:after {
-    content: '';
-    content: none; 
-    } 
-}
-
-q {
-    &:before, &:after {
-    content: '';
-    content: none; 
-    } 
-}
-
-table {
-    border-collapse: collapse;
-    border-spacing: 0; 
-}
-
-button {
-    background-color: transparent;
-    outline: none;
-    border: 0;
-    cursor: pointer; 
-}
+@tailwind utilities;
 `)
 
 			indexts := []byte(`//vendor
@@ -405,8 +289,6 @@ export class ` + componentName + ` extends Component {
             
             root.appendChild(div);
             return {
-                mounted: () => {
-                }
             };
         }
     }
@@ -417,8 +299,6 @@ export class ` + componentName + ` extends Component {
 			err = ioutil.WriteFile("index.html", indexhtml, 0644)
 			check(err)
 			err = ioutil.WriteFile("style.scss", stylescss, 0644)
-			check(err)
-			err = ioutil.WriteFile("_reset.scss", resetscss, 0644)
 			check(err)
 			err = ioutil.WriteFile("index.ts", indexts, 0644)
 			check(err)
